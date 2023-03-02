40510ac00897885adc50d2d3a9e62fd17a8e10f9
luccamicale <luccamicale2015@gmail.com>
2023-03-02 12:58:13 -0300

    ADD REACT

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,15 +1,15 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
+import { Provider } from 'react-redux';
+import store from "./redux/configStore";
 
-function App() {
-  return (<h1>Hello World!</h1>);
-}
-
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+      <App/>
+  </Provider>
 );
